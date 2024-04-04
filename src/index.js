914c088f32596c0488c9229c6af4503ab400d6ec
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-04-04 14:49:28 +0300

    adds films

--- src/index.js
+++ src/index.js
@@ -1,60 +1,82 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const fetchFirstFilm = () => {
-        fetch(url + id)
-        .then(response => response.json())
-        .then(data => { renderFirstFilm(data)})
-    }
-    fetchFirstFilm();
-
-//get elements of poster by ID
-    const renderFirstFilm = (film) => {
-        const poster = document.getElementById('poster')
-        poster.src = film.poster
-    }
-// get movie data or info  by ID
-    const title = document.getElementById('title')
-    title.innerText = film.title
-    const runtime = document.getElementById('runtime')
-    runtime.innerText = `${film.runtime} MINUTES`
-    const showtime  = document.getElementById('showtime')
-    showtime.innerText = film.showtime
-    const description = document.getElementById('film-info')
-    description.innerText = film.description
-
-
-    //tickets sold will be film.capacity string minus film.tickets_sold
-    const ticketSpan = document.getElementById('tikcket-num')
-    const convertedCapacity = parseInt(film.capacity, 10)
-    ticketSpan.innerText = convertedCapacity - film.tickets_sold
-
-    //use querrySelector to identify button
-    const button = document.querySelector('.ui.orange.button')
-    button.addEventListener('click', (e) => {
-       if (ticketSpan.innerText <= 0){
-          let buttonDiv = document.querySelector('.extra.content');    
-           buttonDiv.innerHTML = "Sold Out"
-       }
-      else {
-           const tickets_sold = film.tickets_sold + 1
-           let obj = {tickets_sold} //this has to be key:value pair
-           updateTicketNum(obj)
-        }
-    } )
-    //function updateTicketNum updates variable obj
-    const updateTicketNum = (obj) => { // create variable obj
+document.addEventListener("DOMContentLoaded", () =>{
     
-        options = {
-            method: 'PATCH',
-            headers: { 
-               'content-type': 'application/json',
-               'accept': 'application/json'
-             },
-        body: JSON.stringify(obj)
+    
+    //fetch data from json and add titles to dom
+    fetch("http://localhost:3000/films")
+    .then((data) => data.json())//converting data to json
+    .then((posts) => {
+        console.log(posts);
+        let films = document.getElementById("films")
+        for (let post of posts ){
+            films.innerHTML += `<li>${post.title}</li>`
         }
-        fetch((url + id), options)
-        .then(response => response.json())
-        .then(data => {
-        renderFirstFilm(data)
-        })
+    })
+    
+    // fetch data and add posters 
+    fetch("http://localhost:3000/films")
+    .then((data) => data.json())
+    .then((posts) =>{
+        console.log(posts);
+        let poster = document.getElementById("poster")
+        
+        for (let post of posts){
+            poster.src = `${post.poster}`
+            poster.alt = `${post.title}`
+            poster.innerHTML += `<img src = "${poster.src}" alt = "${poster.alt}">`
         }
+    })
+   
+    //populate film data
+    fetch("http://localhost:3000/films")
+    .then((data) => data.json())
+    .then((posts) =>{
+        console.log(posts);
+        for (let post of posts){
+    const title = document.getElementById("title")
+    title.innerText = post.title
+    const runtime = document.getElementById("runtime")
+    runtime.innerText = `${post.runtime} MINUTES`
+    const showtime = document.getElementById("showtime")
+    showtime.innerText = post.showtime
+    const description = document.getElementById("film-info")
+    description.innerText = `${post.description}`
+   
+//calculate available tickets 
+    const tickets = document.getElementById('ticket-num')
+    const totalTickets = parseInt(post.capacity, 10)
+    tickets.innerText = totalTickets - `${post.tickets_sold}`
+
+    const button = document.getElementById("buy-ticket").addEventListener('click', () => {
+        if (tickets.innerText <= 0){
+            //let buttonDiv = document.querySelector('.extra.content');
+            button.innerHTML = "<button> SOLD OUT </button>"
+        }
+        else {
+            const tickets_sold = post.tickets_sold + 1
+            let obj = {tickets_sold}
+
+            updateTicketNum(obj)
+        }
+        const updateTicketNum = (obj) => {
+            options = {
+                method: 'PATCH',
+                headers: {
+                    "content-type": "application/json",
+                    "accept": "application/json"
+                },
+            body: JSON.stringify(obj)
+            }
+            fetch("http://localhost:3000/films")
+            .then(data => data.json())
+            .then(posts => {
+                renderFirstFilm(posts)
+            
+            })
+        }
+    })
+}
+})
+
+
+        
 })
